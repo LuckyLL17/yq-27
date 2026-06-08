498769f74e87be2ae6903caef2567ba47620cfdf
LuckyLL17 <xiangyujia50@gmail.com>
2026-06-08 17:26:31 +0800

    .1294570642211112:07e9ac39f663c44a8fdfb7ecc63ae51b_6a268367ef6f2d3b1b371958.6a268386ef6f2d3b1b371963.6a2683867d6d8c636a068639:Trae CN.T(2026/6/8 16:55:34)

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-react-components/vite'
+import path from 'node:path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@config': path.resolve(__dirname, './src/config'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
   build: {
     sourcemap: 'hidden',
   },
@@ -21,6 +37,28 @@ export default defineConfig({
           'react-dev-locator',
         ],
       },
+    }),
+    AutoImport({
+      imports: [
+        'react',
+        'react-router-dom',
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: [
+        'src/hooks',
+        'src/store',
+        'src/config',
+        'src/lib',
+      ],
+      eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true,
+      },
+    }),
+    Components({
+      dts: true,
+      local: true,
     }),
     traeBadgePlugin({
       variant: 'dark',
